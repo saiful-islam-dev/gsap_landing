35f7b2fd177a72e6cbbb94f03d08a5326496006a
Saiful264 <saifulislamx264@gmail.com>
2025-07-16 23:16:47 +0600

    Start working on contact section

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -69,7 +69,7 @@ const Art = () => {
 
         <div className="masked-container">
           <h2 className="will-fade">Sip-Worthy Perfection</h2>
-          <div id="masked-content">
+          <div id="masked-content mt-4">
             <h3>Made with Craft, Poured with Passion</h3>
             <p>
               This isn’t just a drink. It’s a carefully crafted moment made just
